d7d627509d8cb2ff64a0c5ad9560f5b85e25b0b9
Jhonatan Diniz <jhonatangomesdiniz@gmail.com>
2023-05-24 07:59:35 -0300

    Populando base de dados

---
+++ src/main/java/com/helpdesk/HelpDesk/config/DevConfig.java
@@ -0,0 +1,22 @@
+package com.helpdesk.HelpDesk.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import com.helpdesk.HelpDesk.services.DBService;
+
+@Configuration
+@Profile("dev")
+public class DevConfig {
+
+	@Autowired
+	private DBService dbService;
+
+	@Bean
+	public void instaciaDB() {
+		this.dbService.instaciaDB();
+	}
+
+}
